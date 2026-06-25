c072a9998f5753cc2b72d7aaacd4a3430548643e
joseph-kt-mbl <youcefabdellaouidev@gmail.com>
2026-06-25 22:06:07 +0100

    =adding multihost and keep tracking containers + fix issues and create final images for dockerhub to be used on k8s Later

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
+    // host: '0.0.0.0',  // Important for Docker
+    port: 5173,
+    allowedHosts: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // your backend
+        target: 'http://backend_app:5000',  // Use container name!
         changeOrigin: true,
         secure: false,
-      },
-    },
-  },
-});
+      }
+    }
+  }
+})
